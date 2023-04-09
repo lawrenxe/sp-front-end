983c685d4f7081ac9961964778b1301b15f86e82
Yizhou Wang <yizhouw7@student.unimelb.edu.au>
2023-04-10 00:49:32 +1000

    Implement the Magic Button

---
+++ src/components/MagicWords/MagicWords.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./MagicWords.css";
+
+export const MagicWords = () => {
+  return (
+    <div className="magic-words">
+      <p className="magic-word">tHEre iS nO MAgIc iN ThE wOrLD,</p>
+      <p className="magic-word">unless you Donate to us.</p>
+    </div>
+  );
+};
